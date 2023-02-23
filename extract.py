fd7f2778cd271e3ab7568a81a178af656458dc77
Bakulin Vycheslav <slava38710505@gmail.com>
2023-02-23 21:28:02 +0700

    Fixed gpu using in CLI

--- extract.py
+++ extract.py
@@ -1,10 +1,10 @@
-
 def logging(text_message = 'Something went wrong'):
     print(text_message)
     
 from datetime import datetime
 import time
 from tqdm.auto import tqdm, trange
+BATCH_SIZE = 6
 
 
 start_loading_time = datetime.now()
@@ -14,7 +14,7 @@ import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 model_name = "models/keyT5-custom" # or 0x7194633/keyt5-base
 tokenizer = T5Tokenizer.from_pretrained(model_name)
-model = T5ForConditionalGeneration.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name).to('cuda:0')
 
 logging(f"The model was loaded in {datetime.now() - start_loading_time}")
 
@@ -28,7 +28,7 @@ class Extractor(object):
     """An Extractor class."""
     
     def extract(self, text, **kwargs):
-        inputs = tokenizer(text, return_tensors='pt')
+        inputs = tokenizer(text, return_tensors='pt').to(model.device)
         with torch.no_grad():
             hypotheses = model.generate(**inputs, num_beams=5, **kwargs)
         s = tokenizer.decode(hypotheses[0], skip_special_tokens=True)
@@ -36,6 +36,21 @@ class Extractor(object):
         s = [el for el, _ in groupby(s)]
         s = list(set(s))
         return s
+    
+    def batch_extract(self, batch_texts, **kwargs):
+        batch_texts = list(batch_texts)
+        inputs = tokenizer(batch_texts, return_tensors='pt', padding=True).to(model.device)
+        with torch.no_grad():
+            hypotheses = model.generate(**inputs, num_beams=5, **kwargs)
+        
+        output = list()
+        for i in range(len(batch_texts)):
+            s = tokenizer.decode(hypotheses[i], skip_special_tokens=True)
+            s = s.replace('; ', ';').replace(' ;', ';').lower().split(';')[:-1]
+            s = [el for el, _ in groupby(s)]
+            s = list(set(s))
+            output.append(s)
+        return output
     
     def text(self, text = "testing/example.txt"):
         if len(text)<5:
@@ -64,17 +79,17 @@ class Extractor(object):
         logging("Extaction was started")
         start_extraction_time = datetime.now()
         
-        batch_size = 4  # сколько примеров показывем модели за один шаг
-        
+        batch_size = BATCH_SIZE
+        is_incomplete_batch = len(test_df)%batch_size != 0
         
         extracted_key_skills = []
-        for i in tqdm(range(len(test_df))):
+        for i in tqdm(range(0, int(len(test_df) / batch_size)+is_incomplete_batch)):
             
+            batch_texts = test_df.iloc[i * batch_size: (i + 1) * batch_size].description
             
-            text = test_df.iloc[i].description
-            extracted = self.extract(text, top_p=1.0, max_length=64)
-            extracted = ';'.join(extracted)
-            extracted_key_skills.append(extracted)
+            batch_extracted = self.batch_extract(batch_texts, top_p=1.0, max_length=64)
+            batch_extracted = [';'.join(extracted) for extracted in batch_extracted]
+            extracted_key_skills += batch_extracted
             
         test_df['extracted_key_skills'] = extracted_key_skills
         logging(f"Extaction was ended in {datetime.now() - start_extraction_time}")
