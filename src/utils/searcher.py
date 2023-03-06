2083b4be8636aff88f047fb676cd713e4bb0437d
Bakulin Vycheslav <slava38710505@gmail.com>
2023-03-06 17:53:22 +0700

    Created base class for searcher, that'll work with diff key_skills sets

---
+++ src/utils/searcher.py
@@ -0,0 +1,66 @@
+
+
+
+from flashtext.keyword import KeywordProcessor
+keyword_processor = KeywordProcessor()
+for key_skill in all_key_skills_counts_filtred.index:
+    keyword_processor.add_keyword(key_skill)
+    keyword_processor.add_keyword(key_skill)
+    
+    
+keywords_found = keyword_processor.extract_keywords('Обязанности:  анализ документации и работа со статистикой; разработка нагрузочных скриптов (Java или C#) ; запуск тестов и анализ результатов, написание отчетов и составление документации; А также: разработка заглушек и эмуляторов, разработка утилит для генерации данных, работа с базами данных.  Требования:  уверенное знание одного из языков программирование: C/C++, Java, C#, Phython; умение работать с базами данных на уровне написания простейших SQL-запросов; знание архитектур приложений (толстый/тонкий клиент); возможность работать полный рабочий день.  Условия:  работа в молодом активном коллективе; конкурентная заработная плата, оформление по ТК РФ; постоянное обучение внутри компании; возможность профессионального роста; участие в тестировании ПО в крупных российских банках.    Первый этап - собеседование по онлайн.')
+print(keywords_found)
+
+
+class Searcher(object):
+    """Searcher class"""
+    
+    def search_in_text(self, text):
+        """
+        Поиск ключевых слов по одному тексту
+        
+        Output:
+        list
+        """
+        pass
+    
+    
+    def search_in_texts(self, texts):
+        """
+        Поиск ключевых слов по коллекции текстов 
+                
+        Output:
+        list[list]
+        """
+        pass
+    
+    
+    def save_skills_collection(self, area_id, key_skills):
+        """
+        Метод сохранения ключевых слов из списка key_skills в общую таблицу для реиспользования                
+        
+        Output:
+        None
+        """
+        pass
+    
+    
+    def load_skills_collection(self, area_id):
+        """
+        Метод инициации поискового алгоритма для определенной области area_id  
+        
+        Output:
+        None
+        """
+        
+        pass
+    
+    
+    def search_by_areas(self, texts, area_ids):
+        """
+        Метод для поиска вакансий с различными колекциями ключевых слов. Для того случая, когда на вход подаются вакансии из разных областей, а для разных областей присутствуют разные ключевые слова
+        
+        Output:
+        list[list]
+        """
+        pass
